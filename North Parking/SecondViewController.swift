91ca5c15cde1accbea4029e7752dac1e9ba1ce5a
Henry Macht <henrymacht@henrys-mbp.fios-router.home>
2018-09-18 16:48:07 -0400

    Initial Commit

---
+++ North Parking/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  North Parking
+//
+//  Created by Henry Macht on 9/18/18.
+//  Copyright © 2018 Henry Macht. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
