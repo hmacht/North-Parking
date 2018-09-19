fe7fb60a02ce179a172a60411c2588a934ea425f
Henry Macht <henrymacht@henrys-macbook-pro.local>
2018-09-18 20:15:14 -0400

    Added location feature Added park button

--- North Parking/FirstViewController.swift
+++ North Parking/FirstViewController.swift
@@ -7,17 +7,73 @@
 //
 
 import UIKit
+import CoreLocation
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, CLLocationManagerDelegate {
+    
+    // VARS
+    var parkButton = UIButton()
+    
+    let screenSize = UIScreen.main.bounds
+    
+    
+    
+    // Create UI
+    
+    func createParkButton(){
+        let screenWidth = CGFloat(screenSize.width)
+        let screenHeight = CGFloat(screenSize.height)
+        let buttonWidth = CGFloat(430)
+        let buttonHeight = CGFloat(300)
+        
+        let image = UIImage(named: "Group 985") as UIImage?
+        parkButton.frame = CGRect(x: screenWidth/2 - buttonWidth/2, y: screenHeight/2 - buttonHeight/2, width: buttonWidth, height: buttonHeight)
+        parkButton.setImage(image, for: .normal)
+        parkButton.addTarget(self, action: "park", for: UIControlEvents.touchUpInside)
+        self.view.addSubview(parkButton)
+    }
+    
+    
+    // LOCATION
+    
+    let locationManager = CLLocationManager()
+    
+    func locationSetup(){
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    // called to pulll gps data
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationSetup()
+        createParkButton()
+        
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // ACTIONS
+    
+    @objc func park() {
+        print("PARK")
     }
 
 
